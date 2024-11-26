fc90bd96ece3fccaf07a7a7ef9fae94ba892b46d
datnydev <dev@datny.art>
2024-11-26 09:43:16 +0100

    data for bsc deploy

--- script/DeployOneMillionCubes.s.sol
+++ script/DeployOneMillionCubes.s.sol
@@ -14,7 +14,8 @@ contract DeployOneMillionCubes is Script {
 
         // Create game configuration parameters
         OneMillionCubes.GameConfig memory gameConfig = OneMillionCubes.GameConfig({
-            fee: 0.0015 ether,
+            fee: 0.0078 ether,
+            //fee: 0.0015 ether,
             minDiscElemNumb: 800000,
             blockTimestamp: 1740484800,
             totalElemNumb: 1000000,
@@ -23,16 +24,30 @@ contract DeployOneMillionCubes is Script {
 
         // Create reward parameters
         // 1 ether ~ 3320 USD
+        //OneMillionCubes.RewardParameters memory rewardParams = OneMillionCubes.RewardParameters({
+        //    singleGoldReward: 300 ether,
+        //    numOfGoldRewards: 3,
+        //    singleSilverReward: 0.15 ether,
+        //    numOfSilverRewards: 500,
+        //    singleBronzeReward: 0.03 ether,
+        //    numOfBronzeRewards: 1000,
+        //    singlePlatinumReward: 0.015 ether,
+        //    numOfPlatinumRewards: 5000,
+        //    singleDiamondReward: 0.003 ether,
+        //    numOfDiamondRewards: 10000
+        //});
+        
+        // 1 BNB ~ 64010 USD
         OneMillionCubes.RewardParameters memory rewardParams = OneMillionCubes.RewardParameters({
-            singleGoldReward: 300 ether,
+            singleGoldReward: 1559 ether,
             numOfGoldRewards: 3,
-            singleSilverReward: 0.15 ether,
+            singleSilverReward: 0.78 ether,
             numOfSilverRewards: 500,
-            singleBronzeReward: 0.03 ether,
+            singleBronzeReward: 0.15 ether,
             numOfBronzeRewards: 1000,
-            singlePlatinumReward: 0.015 ether,
+            singlePlatinumReward: 0.077 ether,
             numOfPlatinumRewards: 5000,
-            singleDiamondReward: 0.003 ether,
+            singleDiamondReward: 0.015 ether,
             numOfDiamondRewards: 10000
         });
 
